cf3e1bb7f23bcffffa530af48bdaa975f2f15019
Lev_TheDev <levinosecretti@outlook.com>
2023-09-02 10:22:36 -0400

    List, enum, compositions.

---
+++ a152_exCompositions/src/application/Program.java
@@ -0,0 +1,66 @@
+package application;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Client;
+import entities.Order;
+import entities.OrderItem;
+import entities.Product;
+import entities.enums.OrderStatus;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		
+		System.out.println("Enter client data:");
+		System.out.print("Name: ");
+		String name = sc.nextLine();
+		System.out.print("Email: ");
+		String email = sc.nextLine();
+		System.out.print("Birth date (DD/MM/YYYY): ");
+		LocalDate birthDate = LocalDate.parse(sc.next(), fmt1);
+		
+		System.out.println("Enter order data:");
+		System.out.print("Status: ");
+		sc.nextLine();
+		String status = sc.nextLine();
+		System.out.print("How many items to this order? ");
+		int n = sc.nextInt();
+		sc.nextLine();
+		
+		LocalDateTime moment = LocalDateTime.now();
+		
+		Order order = new Order(moment, OrderStatus.valueOf(status), new Client(name, email, birthDate));
+		
+		for (int i = 1; i <= n; i++) {
+			System.out.println("Enter #" + i + " item data:");
+			System.out.print("Product name: ");
+			String nameP = sc.nextLine();
+			System.out.print("Product price: ");
+			Double priceP = sc.nextDouble();
+			System.out.print("Quantity: ");
+			Integer quantity = sc.nextInt();
+			sc.nextLine();
+			
+			Product product = new Product(nameP, priceP);
+			OrderItem orderItem = new OrderItem(quantity, priceP, product);
+			order.addItem(orderItem);
+		}
+		
+		System.out.println();
+		System.out.println("ORDER SUMMARY:");
+		System.out.println(order);
+		
+		sc.close();
+	}
+
+}
---
+++ a152_exCompositions/src/entities/Order.java
@@ -0,0 +1,89 @@
+package entities;
+
+import java.text.DecimalFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+import entities.enums.OrderStatus;
+
+public class Order {
+	
+	DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+	DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+	
+	private LocalDateTime moment;
+	private OrderStatus status;
+	
+	private Client client;
+	List<OrderItem> items = new ArrayList<>();
+	
+	public Order() {
+	}
+
+	public Order(LocalDateTime moment, OrderStatus status, Client client) {
+		this.moment = moment;
+		this.status = status;
+		this.client = client;
+	}
+
+	public LocalDateTime getMoment() {
+		return moment;
+	}
+	public void setMoment(LocalDateTime moment) {
+		this.moment = moment;
+	}
+	public OrderStatus getStatus() {
+		return status;
+	}
+	public void setStatus(OrderStatus status) {
+		this.status = status;
+	}
+	public List<OrderItem> getItems() {
+		return items;
+	}
+	public void setItems(List<OrderItem> items) {
+		this.items = items;
+	}
+	public Client getClient() {
+		return client;
+	}
+	public void setClient(Client client) {
+		this.client = client;
+	}
+
+	public void addItem(OrderItem item) {
+		items.add(item);
+	}
+	
+	public void removeItem(OrderItem item) {
+		items.remove(item);
+	}
+	
+	public Double total() {
+		double sum = 0.00;
+		for (OrderItem i : items) {
+			sum += i.subTotal();
+		}
+		return sum;
+	}
+	
+	DecimalFormat fmt3 = new DecimalFormat("0.00");
+	
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("Order moment: " + moment.format(fmt1) + "\n");
+		sb.append("Order status: " + status + "\n");
+		sb.append("Client: " + client.getName() + " (" + client.getBirthDate().format(fmt2) + ") - " + client.getEmail() + "\n");
+		sb.append("Order items: \n");
+		for (OrderItem i : items) {
+			sb.append(i.getProduct() + ", $" + fmt3.format(i.getPrice()) + ", Quantity: " + i.getQuantity() + ", Subtotal: $" + fmt3.format(i.getPrice()*i.getQuantity()) + "\n");
+		}
+		sb.append("Total price: $" + fmt3.format(total()));
+		return sb.toString();
+	}
+	
+	
+
+}
