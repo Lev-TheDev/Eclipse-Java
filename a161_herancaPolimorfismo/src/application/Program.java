dd0426f473036355e583dc54723d5fb4fdef12f4
Lev_TheDev <levinosecretti@outlook.com>
2023-09-23 18:39:16 -0400

    Estudando polimorfismo a161.

---
+++ a161_herancaPolimorfismo/src/application/Program.java
@@ -0,0 +1,61 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Employee;
+import entities.OutsourcedEmployee;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		List<Employee> list = new ArrayList<>();
+		
+		System.out.print("Enter the number of employees: ");
+		int n = sc.nextInt();
+		
+		for (int i=1; i<=n; i++) {
+			System.out.println("Employee #" + i + " data:");
+			System.out.print("Outsourced (y/n)? ");
+			char ch = sc.next().charAt(0);
+			System.out.print("Name: ");
+			sc.nextLine();
+			String name = sc.nextLine();
+			System.out.print("Hours: ");
+			int hours = sc.nextInt();
+			sc.nextLine();
+			System.out.print("Value per hour: ");
+			double valuePerHour = sc.nextDouble();
+			if (Character.toUpperCase(ch) == 'Y') {
+				// StringUtils.capitalize(variável);
+				// StringUtils.lowerCase(variável);
+				System.out.print("Additional charge: ");
+				double additionalCharge = sc.nextDouble();
+				Employee emp = new OutsourcedEmployee(name, hours, valuePerHour, additionalCharge);
+				list.add(emp);
+			}
+			else {
+				Employee emp = new Employee(name, hours, valuePerHour);
+				list.add(emp);
+				// ou eu lanço o list.add com o  new instanciado dentro
+				// list.add(new Employee(name, hours, valuePerHour);
+			}
+		}
+		
+		System.out.println();
+		System.out.println("PAYMENTS: ");
+		
+		for (Employee emp : list) {
+			System.out.println(emp.getName() + " - $ " + String.format("%.2f", emp.payment()));
+		}
+		
+		
+		sc.close();
+	}
+
+}
