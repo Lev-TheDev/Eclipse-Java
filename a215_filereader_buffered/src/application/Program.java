84f7271d9f357b80452a85f4ca604d6a40924c75
Lev_TheDev <levinosecretti@outlook.com>
2023-10-22 14:36:12 -0400

    Estudando TRY-WITH-RESOURCES: File e BufferedReader - a215.

--- a215_filereader_buffered/src/application/Program.java
+++ a215_filereader_buffered/src/application/Program.java
@@ -8,13 +8,8 @@ public class Program {
 
 	public static void main(String[] args) {
 		String path = "/home/rogerlevino/Documentos/a214_filestest/in.txt";
-		FileReader fr = null;
-		BufferedReader br = null;
 		
-		try {
-			fr = new FileReader(path);
-			br = new BufferedReader(fr);
-			
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
 			String line = br.readLine();
 			// readLine() lê uma linha; se a linha for a última linha do texto, retorna null
 			
@@ -25,19 +20,6 @@ public class Program {
 		}
 		catch (IOException e) {
 			System.out.println("Error: " + e.getMessage());
-		}
-		finally {
-			try {
-				if (br != null) {
-					br.close();
-				}
-				if (fr != null) {
-					fr.close();
-				}
-			}
-			catch (IOException e) {
-				e.printStackTrace();
-			}
 		}
 	}
 }
