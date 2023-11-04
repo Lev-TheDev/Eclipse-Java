d6e34a61d52f245482ed2d27042728371d1addc9
Lev_TheDev <levinosecretti@outlook.com>
2023-11-04 18:23:25 -0400

    Heranca multipla - v1.

---
+++ a233_herancamultipla/src/devices/ComboDevice.java
@@ -0,0 +1,11 @@
+package devices;
+
+public class ComboDevice extends Scanner extends Printer {
+	
+	public ComboDevice(String serialNumber) {
+		super(serialNumber);
+	}
+	
+	// não se pode extender de duas classes, mas interfaces sim
+
+}
