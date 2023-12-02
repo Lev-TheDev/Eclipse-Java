ff19a4fb91cf20bad61d023c2c037d1b768d9ea6
Lev_TheDev <levinosecretti@outlook.com>
2023-12-02 10:07:43 -0400

    Estudando Predicate - com classe implementando interface.

--- a254_predicate/src/application/Program.java
+++ a254_predicate/src/application/Program.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import entities.Product;
+import util.ProductPredicate;
 
 public class Program {
 
@@ -15,10 +16,12 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
-		list.removeIf(p -> p.getPrice() >= 100.00);
+		// list.removeIf(p -> p.getPrice() >= 100.00);
 		// remover todo produto p tal que o preço seja maior ou igual a 100.
 		// alternativa:
 		// implementar manualmente classe para explicar
+		
+		list.removeIf(new ProductPredicate());
 		
 		for (Product p : list) {
 			System.out.println(p);
