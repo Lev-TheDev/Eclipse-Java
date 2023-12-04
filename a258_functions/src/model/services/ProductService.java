3848b67906b923d3214082b614a640aaacd0e841
Lev_TheDev <levinosecretti@outlook.com>
2023-12-04 19:59:43 -0400

    Funções - list e lambda como argumentos.

--- a258_functions/src/model/services/ProductService.java
+++ a258_functions/src/model/services/ProductService.java
@@ -1,15 +1,19 @@
 package model.services;
 
 import java.util.List;
+import java.util.function.Predicate;
 
 import entities.Product;
 
 public class ProductService {
 	
-	public double filteredSum(List<Product> list) {
+	public double filteredSum(List<Product> list, Predicate<Product> criteria) {
 		double sum = 0.0;
 		for (Product p : list) {
-			if (p.getName().charAt(0) == 'T') {
+			if (criteria.test(p)) {
+			// if (p.getName().charAt(0) == 'T') {
+				// isto entre parênteses é um predicado, uma condição que retorna V ou F
+				// então posso jogar ele lá no parâmetro do método
 				sum += p.getPrice();
 			}
 		}
