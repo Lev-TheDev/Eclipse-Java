8ac5c33a075d7c7066b01ab5f45e0efffc80a88b
Lev_TheDev <levinosecretti@outlook.com>
2023-12-04 07:16:58 -0400

    a255 - Consumer = implementação da interface por classe.

--- a254_predicate/src/application/Program.java
+++ a254_predicate/src/application/Program.java
@@ -2,12 +2,15 @@ package application;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 import entities.Product;
+import util.PriceUpdate;
 
 public class Program {
 
 	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
 		List<Product> list = new ArrayList<>();
 
 		list.add(new Product("TV", 900.00));
@@ -41,13 +44,16 @@ public class Program {
 		
 		// alternativa4:
 		// expressão lambda inline
-		list.removeIf(p -> p.getPrice() >= 100.00);
-				
+		// list.removeIf(p -> p.getPrice() >= 100.00);
+		// for (Product p : list) {
+		// 	System.out.println(p);
+		// }
+		
+		list.forEach(new PriceUpdate());
+		list.forEach(System.out::println);
+		// reference method para println
 		
 		
-		for (Product p : list) {
-			System.out.println(p);
-		}
 		
 	}
 
