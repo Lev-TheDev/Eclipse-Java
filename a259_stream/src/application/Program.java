b9fed2f8d675c7be0a5122295e05a7f5b54a6140
Lev_TheDev <levinosecretti@outlook.com>
2023-12-05 20:07:03 -0400

    Stream - PIPELINE fluxo de streams.

--- a259_stream/src/application/Program.java
+++ a259_stream/src/application/Program.java
@@ -2,12 +2,13 @@ package application;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Program {
 
 	public static void main(String[] args) {
-		
+		/*
 		// criar stream a partir da lista
 		List<Integer> list = Arrays.asList(3, 4, 5, 10, 7);
 		Stream<Integer> st1 = list.stream().map(x -> x * 10);
@@ -23,6 +24,24 @@ public class Program {
 		
 		Stream<Long> st4 = Stream.iterate(new Long[] {0L, 1L}, p -> new Long[] {p[1], p[0]+p[1]}).map(p -> p[0]);
 		System.out.println(Arrays.toString(st4.limit(20).toArray()));
+		*/
+		
+		List<Integer> list = Arrays.asList(3, 4, 5, 10, 7);
+		Stream<Integer> st1 = list.stream().map(x -> x + 0);
+		System.out.println(Arrays.toString(st1.toArray()));
+		
+		int sum = list.stream().reduce(0, (x, y) -> x + y);
+		// REDUCE = primeiro o elemento neutro da sua operação;
+		// segundo uma função que recebe dois argumentos e gera um resultado (operação).
+		System.out.println("Sum = " + sum);
+		
+		List<Integer> newList = list.stream()
+				.filter(x -> x % 2 == 0)
+				.map(x -> x * 10)
+				.collect(Collectors.toList());
+		System.out.println(Arrays.toString(newList.toArray()));
+		
+		
 	}
 
 }
